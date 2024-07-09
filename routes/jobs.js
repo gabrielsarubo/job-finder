e9b59a92e12e4731d7b41e42360c8845e58d507a
Gabriel Sousa <gabrielsarubo@hotmail.com>
2024-07-09 13:35:27 -0300

    fix: move single job GET route to the end of all the GET requests

--- routes/jobs.js
+++ routes/jobs.js
@@ -4,26 +4,6 @@ const Job = require('../models/Job')
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 
-// get a specific job based on its ID
-router.get('/:id', (req, res) => {
-  const { id } = req.params
-  
-  Job.findOne({
-    where: {
-      id
-    }
-  })
-    .then(job => {
-      if (job) {
-        res.json(job)
-      } else {
-        res.statusMessage = 'There is not a job with that ID'
-        res.sendStatus(404)
-      }
-    })
-    .catch(err => console.log(`Error on retrieving job info with ID ${id}:`, err))
-})
-
 // list all jobs
 router.get('/list', (req, res) => {
   const search = req.query.job
@@ -55,6 +35,26 @@ router.get('/list', (req, res) => {
       })
       .catch(err => console.log('Error on retrieving all jobs with a name filter: ', err))
   }
+})
+
+// get a specific job based on its ID
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  
+  Job.findOne({
+    where: {
+      id
+    }
+  })
+    .then(job => {
+      if (job) {
+        res.json(job)
+      } else {
+        res.statusMessage = 'There is not a job with that ID'
+        res.sendStatus(404)
+      }
+    })
+    .catch(err => console.log(`Error on retrieving job info with ID ${id}:`, err))
 })
 
 // add new job via post
