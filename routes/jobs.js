62d5bfbe798ec39620584767d90582ba6e364674
Gabriel Sousa <gabrielsarubo@hotmail.com>
2024-07-09 10:44:30 -0300

    feat: add GET route for listing all jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,6 +2,18 @@ const express = require('express')
 const router = express.Router()
 const Job = require('../models/Job')
 
+// list all jobs
+router.get('/list', (req, res) => {
+  Job.findAll({
+    order: [
+      ['createdAt', 'DESC']
+    ]
+  })
+    .then(jobs => {
+      res.json(jobs)
+    })
+})
+
 // add new job via post
 router.post('/add', (req, res) => {
   let { title, salary, company, description, email, new_job } = req.body
