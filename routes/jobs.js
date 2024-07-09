94120c10defae613cb72af0dd61735101216345b
Gabriel Sousa <gabrielsarubo@hotmail.com>
2024-07-09 11:14:14 -0300

    feat: find jobs by name

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const router = express.Router()
 const Job = require('../models/Job')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 // list all jobs
 router.get('/list', (req, res) => {
-  Job.findAll({
-    order: [
-      ['createdAt', 'DESC']
-    ]
-  })
-    .then(jobs => {
-      res.json(jobs)
+  const search = req.query.job
+  const query = '%' + search + '%' // PH -> PHP, word -> Wordpress, press -> Wordpress
+
+  if (!search) {
+    Job.findAll({
+      order: [
+        ['createdAt', 'DESC']
+      ]
     })
+      .then(jobs => {
+        res.json(jobs)
+      })
+      .catch(err => console.log('Error on retriving all jobs: ', err))
+  } else {
+    Job.findAll({
+      where: {
+        title: {
+          [Op.like]: query
+        }
+      },
+      order: [
+        ['createdAt', 'DESC']
+      ]
+    })
+      .then(jobs => {
+        res.json(jobs)
+      })
+      .catch(err => console.log('Error on retrieving all jobs with a name filter: ', err))
+  }
 })
 
 // add new job via post
